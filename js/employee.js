611fac8e9f81f0baf13ea8dcd2c7dae9502a0511
akhila.v <akhila.v@qburst.com>
2022-09-29 17:52:40 +0530

    add employee with one skill selected

--- js/employee.js
+++ js/employee.js
@@ -1,58 +1,116 @@
-
-//fetch data
-function fetch_content(url, function_name) {
+//function to fetch and store data
+function fetch_store_content(url, variable_to_store_json) {
     fetch(url)
         .then(response => response.json())
-        .then(list => function_name(list))
+        .then(list => localStorage.setItem(variable_to_store_json, JSON.stringify(list)))
 }
+
 //setting global variable
 let list
 //to display details in table
-fetch_content("../assets/json/employee_list.json", get_employee_details)
+fetch_store_content("../assets/json/employee_list.json", "employee_details")
 
-function get_employee_details(list) {
+get_employee_details()
+
+function get_employee_details() {
     tbody = document.getElementById("tbody")
-    list.forEach(employee => {
-
-        function get_skill_details(skill_list) {
+    JSON.parse(localStorage.getItem("employee_details")).forEach(employee => {
+        console.log(employee);
+        fetch_store_content("../assets/json/skills.json", "skill_details")
+        //   get_skill_details()
+        function get_skill_details() {
+            const skill_details = JSON.parse(localStorage.getItem("skill_details"))
+            console.log(typeof employee.skills);
             const skill_name_array = employee.skills.map(element => {
-                for (let skill_obj of skill_list) {
+
+                for (let skill_obj of skill_details) {
                     if (skill_obj.skill_id == element) {
-                         return skill_obj.skill
+                        return skill_obj.skill
                     }
                 }
             })
             return skill_name_array
         }
 
-    let tr = document.createElement("tr")
-    // tr.innerHTML = ` <td>${employee.employee_id}</td>
-    // <td >${employee.name}</td>
-    // <td>${fetch_content("../assets/json/skills.json",get_skill_details)}</td>
-    // <td>${employee.experience}</td>
-    // <td>
-    // <i id="edit_icon" onclick="open_modal(false)" class="fa-solid fa-user-pen"></i>
-    // <i id="delete_icon" onclick="open_dlt_modal()" class="fa-solid fa-user-slash"></i>
-    // </td>`
-    tbody.appendChild(tr)
+        let tr = document.createElement("tr")
+        tr.setAttribute("class", "table_data")
+
+                                          tr.setAttribute("id", `${employee.employee_id}`)
+        
+        tr.innerHTML = ` <td>${employee.employee_id}</td>
+    <td >${employee.name}</td>
+    <td>${get_skill_details()}</td>
+    <td>${employee.experience}</td>
+    <td>
+    <i id="edit_icon" onclick="open_modal(false)" class="fa-solid fa-user-pen"></i>
+    <i id="delete_icon" onclick="open_dlt_modal(this)" class="fa-solid fa-user-slash"></i>
+    </td>`
+        tbody.appendChild(tr)
     })
+}
+
+//function to add new emp
+function save_new_emp_data() {
+    const employee_id = document.getElementById("emp_id").value
+    const name = document.getElementById("emp_name").value
+    const experience = document.getElementById("emp_exp").value
+    const skills = document.getElementById("emp_skill").value.split(", ")
+//let skills=trial()
+//console.log(temp_skills);
+
+    const phone = document.getElementById("emp_phone").value
+    const email = document.getElementById("emp_mail").value
+    
+    const new_emp_obj = { employee_id, name, experience, skills, phone, email }
+    console.log(new_emp_obj);
+    const employee_details = JSON.parse(localStorage.getItem("employee_details"))
+    employee_details.push(new_emp_obj)
+    localStorage.setItem("employee_details", JSON.stringify(employee_details))
+    remove_old_details()
+    get_employee_details()
+    close_modal()
+
+
+    // function trial (){
+    //     console.log("hi");
+    // const skill_details = JSON.parse(localStorage.getItem("skill_details"))
+    // let skills = temp_skills.map(item => {
+    //     for (let skill_obj of skill_details) {
+    //         if (skill_obj.skill == item) {
+    //             console.log(skill_obj.skill_id);
+    //             return skill_obj.skill_id
+                
+    //         }
+    //     }
+    // })
+    // console.log(skills);
+    // return skills 
+    
+    // }
 
 
 }
 
+function remove_old_details() {
+    document.querySelectorAll('.table_data').forEach(empRow => empRow.remove());
+}
 
-function open_modal(is_add,id_num,emp_obj){
+function open_modal(is_add, id_num, emp_obj) {
     let modal = document.querySelector(".modal");
     modal.style.display = "block";
     let overlay = document.querySelector("#overlay");
     overlay.style.display = "block";
-    if(is_add){
+    if (is_add) {
         let update_heading = document.querySelector("#dynamic_heading")
         update_heading.textContent = "Add Employee";
     }
-    else{
+    else {
         let update_heading = document.querySelector("#dynamic_heading")
-        update_heading.textContent = "Update Employee";       
+        update_heading.textContent = "Update Employee";
+        let input_box = document.querySelectorAll(".input_box")
+        input_box.forEach(content =>{
+            content.value=""
+        })
     }
 }
 
@@ -64,16 +122,22 @@ function close_modal() {
     modal.style.display = "none";
 }
 
-function open_dlt_modal() {
+function open_dlt_modal(target_dlt_icon) {
     let dlt_modal = document.querySelector("#delete_modal");
     let overlay_dlt = document.querySelector("#overlay_delete");
     overlay_dlt.style.display = "block";
     dlt_modal.style.display = "block";
 }
-
 function close_dlt_modal() {
     let dlt_modal = document.querySelector("#delete_modal");
     let overlay_dlt = document.querySelector("#overlay_delete");
     overlay_dlt.style.display = "none";
     dlt_modal.style.display = "none";
 }
+
+
+
+
+
+
+
